c8fa74aad4225303fa20c660b37231559722251b
Steven van Dijk <Steven.vanDijk@dev.adminarsenal.net>
2019-08-21 17:05:24 -0600

    Fixed another regex problem

--- script.js
+++ script.js
@@ -237,13 +237,13 @@ document.addEventListener("DOMContentLoaded", function() {
 function redirectBrokenLinks() {
   if (window.location.href.includes("/knowledge-base/")) {
     let title = window.location.href
-      .match(/\/knowledge-base\//gm)[0]
+      .match(/\/knowledge-base\/.*/gm)[0]
       .replace("/knowledge-base/", "")
       .replace(/-/gm, "+");
     window.location.href = "https://adminarsenal1560455590.zendesk.com/hc/search?query=" + title;
   } else if (window.location.href.includes("/posts/")) {
     let title = window.location.href
-      .match(/\/posts\//gm)[0]
+      .match(/\/posts\/.*/gm)[0]
       .replace("/posts/", "")
       .replace(/-/gm, "+");
     window.location.href = "https://adminarsenal1560455590.zendesk.com/hc/search?query=" + title;
